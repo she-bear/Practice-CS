ab089cb33dc411de69772aaf23f3305bd50db8ae
Olga Kutueva <kutueva.olga@gmail.com>
2022-09-16 17:53:04 +0300

    Task 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// является ли второе число квадратом первого
+Console.Write("Введите число 1: ");
+int number_1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+int number_2 = int.Parse(Console.ReadLine());
+
+int sqr = Convert.ToInt32(Math.Pow(number_2, 2));
+
+if (number_1 == sqr)
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}
