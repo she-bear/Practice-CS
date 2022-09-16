df7011d9c6b7c1fe13e92da03ee494cb51621e4e
Olga Kutueva <kutueva.olga@gmail.com>
2022-09-16 18:35:03 +0300

    Task 2 homework

---
+++ Task_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Поиск максимального из двух чисел: a = 5; b = 7 -> max = 7
+
+Console.Clear();
+Console.Write("Введите число 1: ");
+int number_1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+int number_2 = int.Parse(Console.ReadLine());
+
+if (number_1 == number_2)
+{
+    Console.Write("Числа равны!");
+    return;
+}
+
+if (number_1 > number_2)
+{
+    Console.Write($"Max = {number_1}");
+}
+else
+{
+   Console.Write($"Max = {number_2}");
+}
