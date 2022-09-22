9a6b45200f3bb2dc0beda5cb2cc8d326e262a387
Olga Kutueva <kutueva.olga@gmail.com>
2022-09-22 13:02:09 +0300

    Домашнее задание

---
+++ Task_13/Program.cs
@@ -0,0 +1,18 @@
+﻿// выводит третью цифру заданного числа или сообщает, что третьей цифры нет: 645 -> 5, 78 -> третьей цифры нет, 32679 -> 6
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number > 99)
+{
+    while (number > 1000)
+    {
+        number = number / 10;
+    }
+    
+    Console.WriteLine($"{number % 10}");
+} else
+{
+    Console.WriteLine($"Третьей цифры нет...");
+}
