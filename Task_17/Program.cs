65c9a0788b64e42d897d62247feead3aa6153f60
Olga Kutueva <kutueva.olga@gmail.com>
2022-09-26 19:43:55 +0300

    задачи семинара и домашнее задание

---
+++ Task_17/Program.cs
@@ -0,0 +1,36 @@
+﻿// принимает на вход координаты точки X, Y (причем, X<>0 и Y<>0) и выдает номер четверти плоскости, в которой находится эта точка
+
+Console.Clear();
+Console.Write("Введите X<>0: ");
+int X = int.Parse(Console.ReadLine());
+Console.Write("Введите Y<>0: ");
+int Y = int.Parse(Console.ReadLine());
+
+if (X > 0 && Y > 0)
+{
+    Console.WriteLine($"{X}, {Y}: плоскость 1");    
+}
+
+if (X > 0 && Y < 0)
+{
+    Console.WriteLine($"{X}, {Y}: плоскость 4");    
+}
+
+if (X < 0 && Y < 0)
+{
+    Console.WriteLine($"{X}, {Y}: плоскость 3");    
+}
+
+if (X < 0 && Y > 0)
+{
+    Console.WriteLine($"{X}, {Y}: плоскость 2");    
+}
+
+if (X == 0 || Y == 0)
+{
+    Console.WriteLine($"X, Y не могут быть равны 0!");    
+}
+
+
+
+
---
+++ Task_19/Program.cs
@@ -0,0 +1,29 @@
+﻿// принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+// 14212 -> нет, 12821 -> да, 23432 -> да
+
+Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number < 0)
+{
+    number = -number;
+}
+
+int temp = number;
+int reverse = 0;
+int digit = 0;
+while (temp > 0)
+{
+    digit = temp % 10;
+    reverse = reverse * 10 + digit;
+    temp = temp / 10;
+}
+
+if (number == reverse)
+{
+    Console.WriteLine($"{number} -> палиндром");
+} else
+{
+    Console.WriteLine($"{number} -> нет");
+}
