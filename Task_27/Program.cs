6674f200379c78205e870112986f252b7e1064de
Olga Kutueva <kutueva.olga@gmail.com>
2022-09-30 19:30:41 +0300

    Домашнее задание

---
+++ Task_27/Program.cs
@@ -0,0 +1,18 @@
+﻿// принимает на вход число и выдает сумму цифр в числе: 452 -> 11, 82 -> 10, 9012 -> 12
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"{number} -> {GetCount (number)}");
+
+int GetCount (int number) {
+    int sum = 0;
+    
+    while (number > 0) {
+        
+        int digit = number % 10;
+        number /= 10; // аналог number = number/10; 
+        sum = sum + digit;
+    }
+    
+    return sum;    
+}
---
+++ Task_29/Program.cs
@@ -0,0 +1,53 @@
+﻿// вывод массива из 8 элементов
+Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int array_length = int.Parse(Console.ReadLine());
+
+// способ с random и String.Join
+int [] array = FillArrayRandom(array_length);
+Console.WriteLine($"[{String.Join(",", array)}]");
+
+// способ с вводом с клавиатуры и выводом через метод
+int [] array_2 = FillArrayFromKeyboard(array_length);
+PrintArray(array_2);
+
+
+int [] FillArrayRandom (int size) {
+    int [] collection = new int [size];
+
+    int index = 0;
+    while (index < size)
+    {
+        collection[index] = new Random().Next(0, 101);
+        index++;
+    }
+    
+    return collection;
+}
+
+int [] FillArrayFromKeyboard (int size) {
+    int [] collection = new int [size];
+
+    int index = 0;
+    while (index < size)
+    {
+        Console.Write($"Введите {index}-й элемент массива: ");
+        collection[index] = int.Parse(Console.ReadLine());
+        index++;
+    }
+    
+    return collection;
+}
+
+
+void PrintArray(int[] collection)
+{
+    int count = collection.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{collection[position]}, ");
+        position++;
+    }
+}
+
