b4f7187f7dcd59b08fcc75d63dd0228d1d1801ae
Olga Kutueva <kutueva.olga@gmail.com>
2022-09-30 13:11:20 +0300

    Задачи семинара

---
+++ Task_24/Program.cs
@@ -0,0 +1,14 @@
+﻿// принимает на вход число (A) и выдает сумму чисел от 1 до A: 7 -> 28, 4 -> 10, 8 -> 36
+
+Console.Clear();
+Console.Write ("Введите число: ");
+int limit = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма чисел от 1 до {limit} равна {GetSum (limit)}");
+
+int GetSum (int limit) {
+    int sum = 0;
+    for (int i = 1; i <= limit; i++)
+        sum += i;
+    return sum;    
+}
+
