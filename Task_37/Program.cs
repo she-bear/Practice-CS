64f227e00ff3cc901a12da0a09835c8b0018a57b
Olga Kutueva <kutueva.olga@gmail.com>
2022-10-06 11:12:07 +0300

    задачи семинара

---
+++ Task_37/Program.cs
@@ -0,0 +1,40 @@
+﻿// Произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. 
+// Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3, [6 7 3 6] -> 36 21
+
+Console.Clear();
+Console.Write("Введите элементы массива через пробел: ");
+string elements = Console.ReadLine();
+int[] baseArray = GetArrayFromString(elements);
+
+Console.WriteLine($"[{String.Join(", ", GetMultArray(baseArray))}]");
+
+int[] GetArrayFromString(string stringArray)
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
+
+int[] GetMultArray(int[] collection)
+{
+    // int size = collection.Length / 2;
+    // if (collection.Length % 2 == 1) size++;
+    // вместо двух строк выше можно сделать так:
+     int size = (collection.Length + 1)/ 2;
+
+    int[] result = new int[size];
+
+    for (int i = 0; i < collection.Length / 2; i++)
+    {
+        
+        result[i] = collection[i] * collection[collection.Length - 1 - i];
+    }
+    if (collection.Length % 2 == 1) result[size-1] = collection[collection.Length / 2];
+    return result;
+}
---
+++ Task_38/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+Console.Clear();
+Console.Write("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine());
+
+int[] array = GetRandomArray(size, 0, 999);
+Console.WriteLine($"[{String.Join(",", array)}] -> {MinMaxDifference(array)}");
+
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return result;
+}
+
+int MinMaxDifference (int [] collection)
+{
+    int Min = collection[0];
+    int Max = collection[0];
+
+    foreach (int item in collection)
+    {
+        if (Min > item)
+            Min = item;
+        if (Max < item)
+            Max = item;    
+    }
+
+    return Max - Min;
+}
